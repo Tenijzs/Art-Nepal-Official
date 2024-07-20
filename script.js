226c8607e1b8e8dc02e058a7c7f1be0bc3d9ca17
Tenijzs <botmagar00@gmail.com>
2024-07-20 15:04:57 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn1').addEventListener('click', function() {
+    document.getElementById('aarts').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+    document.getElementById('laast').scrollIntoView({ behavior: 'smooth' });
+});
